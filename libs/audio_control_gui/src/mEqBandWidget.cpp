a9f9d32100ed77537dbfd027c3d92475910a91fd
Maickeli <mimaijala@gmail.com>
2025-12-11 22:05:40 +0200

    Make eq dials work

--- libs/audio_control_gui/src/mEqBandWidget.cpp
+++ libs/audio_control_gui/src/mEqBandWidget.cpp
@@ -5,11 +5,24 @@
 mEqBandWidget::mEqBandWidget(QWidget* parent) : QWidget(parent), ui(new Ui::mEqBandWidget) {
     ui->setupUi(this);
 
-    connect(ui->freq_doubleSpinBox, &QDoubleSpinBox::valueChanged, this, &mEqBandWidget::freqChanged);
-    connect(ui->gain_doubleSpinBox, &QDoubleSpinBox::valueChanged, this, [this]() {
-        qDebug() << "gain changed";
-        emit gainChanged(getGain());
+    ui->freq_dial->setValue(ui->freq_doubleSpinBox->value() * 10000.0f / ui->freq_doubleSpinBox->maximum());
+    ui->gain_dial->setValue(ui->gain_doubleSpinBox->value() * 10000.0f / ui->gain_doubleSpinBox->maximum());
+    ui->q_dial->setValue(ui->q_doubleSpinBox->value() * 10000.0f / ui->q_doubleSpinBox->maximum());
+
+    connect(ui->freq_dial, &QDial::valueChanged, this, [this]() {
+        ui->freq_doubleSpinBox->setValue(ui->freq_dial->value() / 10000.0f * ui->freq_doubleSpinBox->maximum());
     });
+
+    connect(ui->gain_dial, &QDial::valueChanged, this, [this]() {
+        ui->gain_doubleSpinBox->setValue(ui->gain_dial->value() / 10000.0f * ui->gain_doubleSpinBox->maximum());
+    });
+
+    connect(ui->q_dial, &QDial::valueChanged, this, [this]() {
+        ui->q_doubleSpinBox->setValue(ui->q_dial->value() / 10000.0f * ui->q_doubleSpinBox->maximum());
+    });
+
+    connect(ui->freq_doubleSpinBox, &QDoubleSpinBox::valueChanged, this, &mEqBandWidget::freqChanged);
+    connect(ui->gain_doubleSpinBox, &QDoubleSpinBox::valueChanged, this, &mEqBandWidget::gainChanged);
     connect(ui->q_doubleSpinBox, &QDoubleSpinBox::valueChanged, this, &mEqBandWidget::qChanged);
 }
 
